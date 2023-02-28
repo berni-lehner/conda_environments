c9487b433d306853757e9a05d2d5d2e49fe26928
berni-lehner <71018765+berni-lehner@users.noreply.github.com>
2023-02-28 17:14:00 +0100

    add batch files

---
+++ install_packages_ubuntu_py310_20230228.sh
@@ -0,0 +1,8 @@
+conda install -y albumentations -c conda-forge
+conda install scipy
+conda install -y tensorflow
+conda install -y pytorch torchvision torchaudio -c pytorch
+conda install -y jupyter pandas seaborn -c conda-forge
+conda install -y -c conda-forge natsort kneed pandas-profiling pyinstaller pyinstaller-hooks-contrib pytorch-model-summary shapely sweetviz tqdm umap-learn xgboost scikit-image scikit-learn imbalanced-learn tabulate yapf skorch optuna celluloid scikit-learn-intelex pre-commit
+conda install -y -c plotly plotly
+pip install git+https://github.com/berni-lehner/zippee-ki-yay.git
