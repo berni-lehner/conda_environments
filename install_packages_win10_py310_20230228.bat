92a9861f2ba38626fdf550658ff32223ee3a4f5e
Bernhard Lehner <71018765+berni-lehner@users.noreply.github.com>
2023-03-01 14:53:59 +0100

    added scikit-optimize

--- install_packages_win10_py310_20230228.bat
+++ install_packages_win10_py310_20230228.bat
@@ -1,6 +1,6 @@
 cmd /c conda install -y tensorflow-gpu
 cmd /c conda install -y pytorch torchvision torchaudio -c pytorch
 cmd /c conda install -y jupyter pandas scipy seaborn -c conda-forge
-cmd /c conda install -y -c conda-forge natsort kneed pandas-profiling pyinstaller pyinstaller-hooks-contrib pytorch-model-summary shapely sweetviz tqdm umap-learn xgboost scikit-image scikit-learn tabulate yapf albumentations skorch optuna celluloid scikit-learn-intelex
+cmd /c conda install -y -c conda-forge natsort kneed pandas-profiling pyinstaller pyinstaller-hooks-contrib pytorch-model-summary shapely sweetviz tqdm umap-learn xgboost scikit-image scikit-learn scikit-optimize tabulate yapf albumentations skorch optuna celluloid scikit-learn-intelex
 cmd /c conda install -y -c plotly plotly
 
